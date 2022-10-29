24ba389d28fe1e05b24f8413e2885fd7a5ecb67b
sepulzera <hartung_spam@gmx.de>
2022-10-29 20:06:41 +0200

    Refactor CustomServings to use react-final-form

--- src/recipe/components/IngredientsHeader.tsx
+++ src/recipe/components/IngredientsHeader.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 import { Button, Form } from 'react-bootstrap';
+import { Form as ReduxForm } from 'react-final-form';
 
 import { Recipe } from '../store/RecipeTypes';
 
 // import IngredientButtons from './IngredientButtons';
 import { PendingState, ReducerMeta } from '../../common/store/GenericReducerType';
-import Input from '../../common/components/Input/Input';
-import { updateFormData } from '../../common/utility';
 import Icon from '../../common/components/Icon';
+import InitialValuesResetter from '../../common/components/ReduxForm/ReInitialValuesResetter';
+import ReInput from '../../common/components/ReduxForm/ReInput';
 
 export interface IIngredientsHeaderProps {
   recipe:      Recipe | undefined;
@@ -30,7 +31,8 @@ export interface IFormDataProps {
   servings: number;
 }
 
-const IngredientsHeader: React.FC<IIngredientsHeaderProps> = ({ recipe, recipeMeta, updateServings }: IIngredientsHeaderProps) => {
+const IngredientsHeader: React.FC<IIngredientsHeaderProps> = ({
+    recipe, recipeMeta, updateServings }: IIngredientsHeaderProps) => {
   const { formatMessage } = useIntl();
   const messages = defineMessages({
     ingredients: {
@@ -56,24 +58,12 @@ const IngredientsHeader: React.FC<IIngredientsHeaderProps> = ({ recipe, recipeMe
   });
 
   const customServings = recipe?.customServings;
-  const [formData, setFormData] = useState<IFormDataProps>({ servings: customServings ?? 0 });
 
-  useEffect(() => {
-    if (customServings != null && customServings !== formData.servings) {
-      setFormData(prev => updateFormData(prev, 'servings', customServings.toString()));
-    }
-  }, [customServings]);
+  const handleSubmit = (form: IFormDataProps) => updateServings(form.servings);
 
-  const handleChange = (name: string, value: unknown) => {
-    setFormData(prev => updateFormData(prev, name, value));
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (typeof formData.servings === 'string'
-        || customServings === formData.servings) return;
-    updateServings(formData.servings);
-  };
+  const initialValues: Partial<IFormDataProps> = useMemo(() => ({
+    servings: customServings,
+  }), [customServings]);
 
   const pending  = recipeMeta.pending;
   const servings = recipe?.customServings ?? 0;
@@ -91,21 +81,26 @@ const IngredientsHeader: React.FC<IIngredientsHeaderProps> = ({ recipe, recipeMe
             <span className='print-only'>{`: ${recipe?.customServings ?? ''} ${formatMessage(messages.servings)}`}</span>
           </h2>
           <div className='custom-servings print-hidden'>
-            <Form onSubmit={handleSubmit} className='custom-servings'>
-              <Input
-                  name  = 'servings'
-                  type  = 'number'
-                  value = {formData.servings}
-                  aria-label = {formatMessage(messages.servings_input_tooltip)}
-                  min   = {0}
-                  max   = {1000}
-                  autoComplete = 'off'
-                  onChange = {handleChange} />
-              <Button type='submit' variant='primary'>
-                <Icon icon='arrow-repeat' variant='light' />
-                {formatMessage(messages.servings)}
-              </Button>
-            </Form>
+            <ReduxForm
+                initialValues = {initialValues}
+                onSubmit = {handleSubmit}
+                subscription = {{}}
+                render = {({ form, handleSubmit: renderSubmit }) => (
+                  <Form onSubmit={renderSubmit} className='custom-servings'>
+                    <InitialValuesResetter form={form} initialValues={initialValues} />
+                    <ReInput
+                        name  = 'servings'
+                        type  = 'number'
+                        aria-label = {formatMessage(messages.servings_input_tooltip)}
+                        min   = {0}
+                        max   = {1000}
+                        autoComplete = 'off' />
+                    <Button type='submit' variant='primary'>
+                      <Icon icon='arrow-repeat' variant='light' />
+                      {formatMessage(messages.servings)}
+                    </Button>
+                  </Form>
+                )} />
           </div>
         </div>
       )}
